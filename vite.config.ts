9a84299a50c5c0020ae9019cb66873dcdc193200
carrot2803 <lezamadmitri@gmail.com>
2025-04-29 23:24:51 -0400

    fix routing

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	base: "/cvd-app/",
+	base: "/cvd-app",
 });
